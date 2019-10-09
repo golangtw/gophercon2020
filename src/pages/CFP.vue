963ded1e055a8158f1cc606fceb8c7f07c8ebd19
Aries Cs <aries0d0f@gmail.com>
2019-10-09 23:10:29 +0800

    [Optimize] Auto detect meta:og to improve SEO.

--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -21,5 +21,18 @@ import Banner from '@/components/Banner.vue';
     Banner
   }
 })
-export default class CFP extends Vue {}
+export default class CFP extends Vue {
+  public mounted () {
+    const title = document.querySelector('head>title');
+    const metaList = Array.from(document.querySelectorAll('head>meta'));
+
+    if (title) {
+      title.innerHTML = 'Call of Paper｜徵稿開催 — COSCUP 2019 來台講';
+    }
+
+    metaList[3].setAttribute('content', 'Call of Paper｜徵稿開催 — COSCUP 2019 來台講');
+    metaList[5].setAttribute('content', 'https://coscup.org/taigi2019/cfp/');
+    metaList[6].setAttribute('content', 'https://coscup.org/taigi2019/img/cfp-og.png');
+  }
+}
 </script>
