cade42ddf42a6fb7b4e3e5246bf54c1fca20b10b
Aries Cs <aries0d0f@gmail.com>
2019-10-09 13:02:20 +0800

    [Layout] Add `CFP` component.

--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="cfp" class="main-container">
-    
+    <Banner/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Banner from '@/components/Banner.vue';
 
-@Component({})
+@Component({
+  components: {
+    Banner
+  }
+})
 export default class CFP extends Vue {}
 </script>
