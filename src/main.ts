b79d2ea8d79f1254429a3d63c510ce6363c6a6df
Aries Cs <aries0d0f@gmail.com>
2019-10-09 14:19:19 +0800

    [Deploy] Add font-awesome-icon packages.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab);
+library.add(fas);
+library.add(far);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
