3c03a8443d67c9fd08f585318191bd42d91eb1c4
Aries Cs <aries0d0f@gmail.com>
2019-10-09 22:50:35 +0800

    [Layout] Add RWD.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -14,24 +14,24 @@
       >
         Call of Paper｜徵稿開催
       </h3>
-      <h1
-        class="title bold"
-        v-html="`COSCUP 2019 ${parser('來/Laí/台/Taí/講/Gàng/')}`"
-      />
+      <h1 class="title bold">
+        <span>COSCUP 2019 </span>
+        <span v-html="parser('來/Laí/台/Taí/講/Gàng/')"/>
+      </h1>
       <h2 
         class="subtitle"
         v-html="parser('來/Laí/用/Yòng/台/Taí/語/Gí/講/Gong/開/Kai/源/Yuán/')"
       />
+      <p class="content">
+        <span class="date">Dec. 21, 2019</span>
+        <span class="place"><font-awesome-icon :icon="['fas', 'map-marker-alt']"/> 臺中科技大學</span>
+      </p>
       <h2
         v-if="$route.name === 'CFP'"
         class="button"
       >
         <a v-html="parser('我/Wǒ/要/Yaò/投/Toú/稿/Gaǒ/')"></a>
       </h2>
-      <p class="content">
-        <span class="date">Dec. 21, 2019</span>
-        <span class="place"><font-awesome-icon :icon="['fas', 'map-marker-alt']"/> 臺中科技大學</span>
-      </p>
     </header>
   </div>
 </template>
@@ -66,14 +66,25 @@ export default class Banner extends Vue {
   private measureSightSize () {
     const sightContainer: any = this.$refs.sightContainer;
 
-    this.setSight({
-      width: sightContainer.clientWidth,
-      height: sightContainer.clientHeight,
-      spot: {
-        x: sightContainer.clientWidth / 2.0 + 140,
-        y: sightContainer.clientHeight / 2.0 + 50
-      }
-    });
+    if (sightContainer.clientWidth > 840) {
+      this.setSight({
+        width: sightContainer.clientWidth,
+        height: sightContainer.clientHeight,
+        spot: {
+          x: sightContainer.clientWidth / 2.0 + 140,
+          y: sightContainer.clientHeight / 2.0 + 50
+        }
+      });
+    } else {
+      this.setSight({
+        width: sightContainer.clientWidth,
+        height: sightContainer.clientHeight,
+        spot: {
+          x: sightContainer.clientWidth + 50,
+          y: sightContainer.clientHeight / 2.0 + 50
+        }
+      });
+    }
   }
 }
 </script>
