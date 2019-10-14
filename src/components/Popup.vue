272f45f387ca982d9779f6dd83dc765b48c6ce7b
Aries Cs <aries0d0f@gmail.com>
2019-10-14 18:27:45 +0800

    [Layout] Add `Popup` component & stylesheets.

---
+++ src/components/Popup.vue
@@ -0,0 +1,34 @@
+<template>
+  <main
+    v-if="!isOpen"
+    class="popup-container"
+  >
+    <div class="popup-close">
+      <font-awesome-icon :icon="['fas', 'times']"/>
+    </div>
+    <article
+      v-html="content"
+      class="popup-content"
+    />
+  </main>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  }
+})
+export default class Popup extends Vue {
+}
+</script>
