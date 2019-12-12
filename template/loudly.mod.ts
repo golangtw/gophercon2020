692b5088468c46d515f63db38a06b0b9f15aca1c
Aries Cs <aries0d0f@gmail.com>
2019-12-12 12:16:16 +0800

    [Optimize] Isolated popup-info contents as template modules.

---
+++ template/loudly.mod.ts
@@ -0,0 +1,10 @@
+// tslint:disable: max-line-length
+
+// !WARNING! DO NOT EDIT THIS TEMPLATE EXCEPT YOU ARE UNDERSTAND WHAT YOU ACTUALLY DO.
+// This is a "大鳴大放" template block, edit this and it will auto inject contents as popup-ready DOMElement.
+
+const content = `
+<h1 class="title font-black">籌備中，敬請期待</h1>
+`;
+
+export default content;
---
+++ template/openSubmit.mod.ts
@@ -0,0 +1,104 @@
+// tslint:disable: max-line-length
+
+// !WARNING! DO NOT EDIT THIS TEMPLATE EXCEPT YOU ARE UNDERSTAND WHAT YOU ACTUALLY DO.
+// This is a "Open Submit" template block, edit this and it will auto inject contents as popup-ready DOMElement.
+
+const content = `
+<ul class="summary">
+  <li><a href="#important-time">重要時程</a></li>
+  <li><a href="#example">投稿主題</a></li>
+  <li><a href="#coc">Code of Conduct 行為準則</a></li>
+  <li><a href="#session-type">議程種類</a></li>
+  <li><a href="#paper-rule">投稿方式</a></li>
+  <li><a href="#paper-trail">審稿方式</a></li>
+  <li><a href="#notice">投稿注意事項</a></li>
+</ul>
+<h1 id="important-time" class="title font-black">重要時程</h1>
+<ul>
+  <li>2019/12/10（二）投稿開始</li>
+  <li>2020/01/02（四）早上 06:41 投稿截止</li>
+  <li>一月下旬審稿結束、隨後公佈完整議程</li>
+  <li>2020/03/28（六）SITCON 2020</li>
+</ul>
+<h1 id="example" class="title font-black">投稿主題</h1>
+<p class="paragraph">過往的議程都是從講者對聽眾單向式的演講，而開放式議程跳脫以往都是從講者對聽眾單向式的資訊傳遞，期待藉由主講者與與會者的雙向交流，碰撞出更多的觀點與討論。在開放式議程，你可以主持一個圓桌會議，一起腦力激盪，甚至辦一場競賽，任何你想得到的非傳統議程形式，都可以在開放式議程中進行。  </p>
+<p class="paragraph">我們特別期待以學生為主體的投稿，可以是從學生角度出發的經驗分享、技術分享，或是探討學生相關議題等等；廣義而言，只要是與資訊技術、電腦相關的講題，都歡迎投稿！主題可以是：新課綱的現狀與困難、程式相關科目的家教市場、自由軟體滋潤不同領域的成果、女性與 LGBTQ 族群在資訊學術界或產業的處境等。</p>
+<h1 id="coc" class="title font-black">Code of Conduct 行為準則</h1>
+<p class="paragraph">SITCON 非常歡迎女性、跨性別與多元背景的投稿者。為了讓大家都能愉快的參加 SITCON，我們鼓勵所有參與者閱讀年會的<a href="https://sitcon.org/code-of-conduct/" class="font-bold" target="_blank" rel="noopener noreferrer">行為守則（Code of Conduct）</a>，共同創造一個友善的環境。</p>
+<h1 id="session-type" class="title font-black">格式</h1>
+<p class="paragraph">除有特別標示會公布的資料外，其餘則只會供審稿委員閱讀。</p>
+<ul>
+  <li>題目 Title：必填、公開。20 字以下。</li>
+  <li>摘要 Abstract：必填、公開。建議含標點符號 150 ~ 250 全形字，使用於宣傳以及網站。</li>
+  <li>大綱 Outline：必填，請說明該開放式議程的大綱、如何進行、時間長度規劃。</li>
+  <li>目標受眾 Target Audience：必填，請說明您期待該議程的目標受眾，為怎麼樣的人。例如：「對網路治理有興趣的人」、「不知道該不該讀研究所的學生」等。</li>
+  <li>先備知識 Prior Knowledge：必填、公開。請說明該議程的與會者所需具備的先備知識。例如：「能理解 Python 基礎語法」、「熟悉 Machine Learning 演算法」等。</li>
+  <li>詳細說明 Description：必填，不限字數，請說明這個議程你認爲重要的相關細節，以利審稿委員更瞭解你的作品。我們強烈建議您詳細填寫此項，以增加入選機率。</li>
+  <li>附件 Attachment：選填。可以附上程式碼、投影片草稿或任何你認爲有助於我們瞭解你的議程的附件。</li>
+</ul>
+<h2 class="subtitle font-bold">個人資料</h2>
+<p class="paragraph">除暱稱／姓名會被用於宣傳，其餘則只會供議程組聯絡您使用，不會在未經您允許下給第三方。</p>
+<ul>
+  <li>電子郵件 Email：能讓大會聯絡的電子信箱。</li>
+  <li>暱稱／名字 Name：公開於講者海報及網站的稱呼。</li>
+  <li>電話號碼 Phone：用於大會聯絡、確認資訊用的電話號碼。</li>
+  <li>其他聯絡方式 Other contact information：其他任何我們能聯絡您的方式。</li>
+</ul>
+<h1 id="paper-trail" class="title font-black">審稿方式</h1>
+<p class="paragraph">審稿委員主要針對以下項目進行篩選：</p>
+<ul>
+  <li>稿件內容：包含的知識分享、經驗案例、想法觀點是否獨特等。</li>
+  <li>表達能力：提供的資料是否有條理、文句暢通，提供資料（尤其是稿件的詳細說明）之完整度。我們偏好完整的資料，讓審稿委員更清楚了解演講細節。</li>
+  <li>適合與會者：稿件是否適合 SITCON 大多數的與會者討論。</li>
+</ul>
+<h1 id="notice" class="title font-black">投稿注意事項</h1>
+<ul>
+  <li>SITCON 2020 採用 Google Forms 接收投稿。</li>
+  <li>議程時間長度以 40 分鐘為原則。</li>
+  <li>L除了開放式議程外，同一時間將有 3 至 4 軌一般議程同時進行。</li>
+  <li>上述審稿方式不考慮稿件內容的技術難度，我們仍以稿件是否能夠吸引人、高品質、適合與會者（即上述篩選方式）為考量。</li>
+  <li>為鼓勵投稿，大會提供投稿者一組邀請碼 （一人一組，非一稿一組），可優先報名參與本年會。獲選講者請使用講者身分入場，不需要報名，並可將邀請碼轉移他人使用。</li>
+  <li>在截稿之前，投稿者可以隨時使用投稿系統修改已投的稿件。</li>
+  <li>凡稿件經接受者，年會將頒予感謝狀以表彰講者之熱情付出與貢獻。</li>
+  <li>「議程題目」、「摘要」與「先備知識」會放置於官網以及會前發行的電子報中。</li>
+</ul>
+<p class="paragraph">
+  若有任何問題或需投稿建議請 Email 至 SITCON 議程組信箱 <a href="mailto:session@sitcon.org" class="font-bold">session@sitcon.org</a>。
+</p>
+<h2 class="subtitle font-bold">錄影以及記錄</h2>
+<p class="paragraph">所有議程皆會錄影，若不希望被錄影與釋出可以與議程組聯絡。</p>
+<h2 class="subtitle font-bold">授權</h2>
+<p class="paragraph">議程錄影會在經過您的同意後一律以 CC - BY 3.0 授權在 YouTube 釋出。</p>
+<p class="paragraph">議程相關素材，如投影片等，將由您自行決定在何處以何種方式釋出，或是選擇不釋出，我們不會干涉。年會後會向您蒐集素材連結，若您選擇釋出，我們會將連結放置在官網的議程表上。</p>
+<h2 class="subtitle font-bold">演練</h2>
+<p class="paragraph">「演練」讓講者在演講前有練習的機會，我們會與您討論開放式議程進行的方式並進行演練。講者可以自由參加，入選後將由議程組與您協調精確時間。</p>
+<p class="paragraph">所有場次將依報名情況舉辦，並提供投影機、音響、簡報筆、倒數計時器等設備。預計舉辦北部、中部、南部、東部場等，時間約為二月份或三月初，地點另行通知。</p>
+<p class="paragraph">補助：依照客運價格標準補助講者至最近演練場地的全額或部分車馬費。</p>
+<h2 class="subtitle font-bold">注意</h2>
+<ul>
+  <li>除非內容明顯偏離講題或不符事實，我們不會要求講者對演講內容進行修正。</li>
+  <li>為了維護所有講者的權利，試講時間恕不接受更改，請各位講者者準時參與。</li>
+</ul>
+<h2 class="subtitle font-bold">Q&amp;A</h2>
+<p class="paragraph">
+  Q：我想在開放式議程嘗試 XXX 可以嗎？<br>
+  A：原則上只要是跳脫傳統演講形式，以雙向互動為主的議程進行方式都是可以的，但必須遵守 CoC，且建議議程以知識與想法交流為優先。
+</p>
+<p class="paragraph">
+  Q：先備知識與目標受眾有何不同？<br>
+  A：「目標受眾」表示您預期何種人將會前來參與該場議程，此欄位可能表示為會眾的興趣、生活環境抑或年齡層。「先備知識」表示對該議程有興趣者，您建議須先備哪些基礎技術、能力或經驗，有助於了解與吸收該場議程的知識精華。舉例而言，若一講題為「在營隊中使用虛擬貨幣」，則先備知識可能是「沒有」，目標受眾可能是「有想要辦營隊的人」。而如果是有關 Julia 程式語言的演講，則先備知識可能是「知道平行運算是什麼」，目標受眾可能是「想用 Julia 做資料科學的人」。
+</p>
+<p class="paragraph">
+  Q：投稿主題是否有必要切合年會 slogan（#define student）？<br>
+  A：不用。審稿時僅會依照上文所述方式審查，不會因為切合 slogan 就拿到比較高分，或是與 slogan 無關就被扣分。
+</p>
+<p class="paragraph">
+  Q：還有疑問怎麼辦？<br>
+  A：歡迎來投稿者小聚與我們聊聊，或是寄信至 <a href="mailto:session@sitcon.org" class="font-bold">session@sitcon.org</a> 詢問，我們會儘速回應。
+</p>
+<a href="https://forms.gle/VeSHAk1pohNi9bZb7" class="submission button font-black" target="_blank" rel="noopener noreferrer">
+  <p>我要投稿</p>
+</a>
+`;
+
+export default content;
