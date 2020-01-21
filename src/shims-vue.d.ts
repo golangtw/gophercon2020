31b235383737e85dcf2de1823e3288ec933fc7b2
Aries Cs <aries0d0f@gmail.com>
2020-01-21 18:22:15 +0800

    [Deploy] Add OPass session json script & OPass session table modules.

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,7 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
+
+declare module '@ccip-app/session-table';
+
+declare module 'spreadsheet-to-json';
---
+++ src/util/session/config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.spreadsheetKey = "18PleK7TU6zseEnujAp6c9RSb4SkK9PZ2MUfd5N2iMPw"
+// config.spreadsheetKey = "1QsUPjlG5aKsVk5bpCYO4Ssig8MoQmVmd1SCVO_pGrdY"
+
+config.default_avatar = "https://sitcon.org/2019/static/img/staffs/stone.png"
+config.avatar_base_url = "https://sitcon.org/2019/static/img/speaker/"
+
+module.exports = config
