b4f44b7ae6fc611487bf0127cea9353d6342f85a
Aries Cs <aries0d0f@gmail.com>
2020-01-21 19:08:22 +0800

    [Layout] Add session component.

---
+++ src/pages/Session.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="session" class="main-container">
+    <CCIPSessionTable
+      :sessionData="sessionData"
+      :rooms="['R2', 'R0', 'R1', 'R3', 'S']"
+      :isMobile="isMobile()"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Getter } from 'vuex-class';
+import CCIPSessionTable from '@ccip-app/session-table';
+
+import sessionData from '../../public/json/session.json';
+
+import { DeviceType } from '../store/types/app';
+
+@Component({
+  components: {
+    CCIPSessionTable
+  }
+})
+export default class Session extends Vue {
+  @Getter('device', { namespace: 'app' })
+  private device!: DeviceType;
+
+  private sessionData = sessionData;
+
+  private isMobile (): boolean {
+    return this.device === DeviceType.MOBILE;
+  }
+}
+</script>
