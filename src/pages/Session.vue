a2f16f48907e22eb00a8c29dfb4a29f79d3252e5
Aries Cs <aries0d0f@gmail.com>
2020-01-22 22:48:57 +0800

    [Layout] Stream session popup.

--- src/pages/Session.vue
+++ src/pages/Session.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="session" class="main-container">
+  <div 
+    id="session"
+    class="main-container"
+    :class="{ 'popuped': isPopup }"
+  >
     <CCIPSessionTable
       :sessionData="sessionData"
       :rooms="['R2', 'R0', 'R1', 'R3', 'S']"
       :isMobile="isMobile()"
-      :isPopup.sync="isPopup"
+      :isPopup.sync="popUp"
       :popUpSession.sync="popUpSession"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { Getter } from 'vuex-class';
+import { Component, Watch, Vue } from 'vue-property-decorator';
+import { Action, Getter } from 'vuex-class';
 import CCIPSessionTable from '@ccip-app/session-table';
 
-import sessionData from '../../public/json/session.json';
+import sessionData from '@/../public/json/session.json';
+import sessionDOMString from '@/../template/session.mod';
 
-import { DeviceType } from '../store/types/app';
+import { DeviceType } from '@/store/types/app';
 
 @Component({
   components: {
@@ -25,15 +30,46 @@ import { DeviceType } from '../store/types/app';
   }
 })
 export default class Session extends Vue {
-  @Getter('device', { namespace: 'app' })
-  private device!: DeviceType;
+  @Action('toggleTheme', { namespace: 'app' }) private toggleTheme!: () => void;
+  @Action('togglePopup', { namespace: 'app' }) private togglePopup!: (status: boolean) => void;
+  @Action('togglePopupContent', { namespace: 'app' }) private togglePopupContent!: (content: string) => void;
+  @Getter('isPopup', { namespace: 'app' }) private isPopup: any;
+  @Getter('device', { namespace: 'app' }) private device!: DeviceType;
 
   private sessionData = sessionData;
-  private isPopup = false;
+  private popUp = false;
   private popUpSession = {};
+
+  @Watch('popUp')
+  public onChangeInnerPopup (newVal: boolean) {
+    this.processPopup(newVal);
+  }
+
+  @Watch('isPopup')
+  public onChangePopup (newVal: boolean) {
+    this.processPopup(newVal);
+  }
 
   private isMobile (): boolean {
     return this.device === DeviceType.MOBILE;
+  }
+
+  private processPopup (isPopup: boolean): void {
+    if (isPopup) {
+      this.togglePopup(true);
+
+      const result = JSON.parse(JSON.stringify(this.popUpSession as any));
+      result.speakers = result.speakers.map((id: string) => (JSON.parse(JSON.stringify(this.getSpeaker(id)))));
+
+      this.togglePopupContent(sessionDOMString(result));
+    } else {
+      this.popUp = false;
+      this.popUpSession = {};
+    }
+  }
+
+  private getSpeaker (id: string): any {
+    return this.sessionData.speakers.find((speaker) => (speaker.id === id));
   }
 }
 </script>
