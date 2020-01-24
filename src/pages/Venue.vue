56eb4bf7129386e2bd98e0b9ef155c5245fcf92b
hyperbola <hyperbola.cs07@gmail.com>
2020-01-24 13:58:50 +0800

    Activity bar not done ...

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -1,17 +1,13 @@
 <template>
-  <div
-    id="venue"
-    class="main-container"
-    :class="{ 'popuped': isPopup }"
-  >
+  <div id="venue" class="main-container">
     <main class="spotlight-container">
       <div class="spotlight">
         <h1 class="bold">活動地圖</h1>
-        <ActivityBar></ActivityBar>
-        <p>2020 的 svg 下載不了，先用 2019 代替</p>
+        <ActivityBar />
         <!-- 2020 的 svg 下載不了，先用 2019 代替 -->
         <img src="@/../public/img/venue-3f.svg" alt="3樓場地平面圖" />
         <img src="@/../public/img/venue-4f.svg" alt="4樓場地平面圖" />
+        <p>2020 的 svg 下載不了，先用 2019 代替</p>
       </div>
     </main>
     <Footer />
@@ -22,20 +18,22 @@
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/CFP/Banner.vue';
 import Footer from '@/components/CFP/Footer.vue';
+import ActivityBar from '@/components/ActivityBar.vue';
 
 @Component({
   components: {
     Banner,
-    Footer
+    Footer,
+    ActivityBar
   }
 })
 export default class Venue extends Vue {}
 </script>
 
 <style scoped>
-    img {
-        position: relative;
-        width: 100%;
-        left: 2.5%;
-    }
+img {
+  position: relative;
+  max-width: 100%;
+  margin: 5% 5% 5% 7%;
+}
 </style>
