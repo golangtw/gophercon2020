86eee44f9b4b5d4c414619942e3adeb1eea33143
Aries Cs <aries0d0f@gmail.com>
2020-01-27 17:27:46 +0800

    [Layout] Add Venue component and stylesheets.

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -1,11 +1,27 @@
 <template>
-  <div id="venue" class="main-container"/>
+  <div id="venue" class="main-container">
+    <h1 class="title">會場地圖</h1>
+    <Map3F class="map"/>
+    <Map4F class="map"/>
+    <div class="floor-box">
+      <p @click="$router.push({ hash: '#map-3F' })">3F</p>
+      <p @click="$router.push({ hash: '#map-4F' })">4F</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component({})
+import Map3F from '@/components/Venue/Map3F.vue';
+import Map4F from '@/components/Venue/Map4F.vue';
+
+@Component({
+  components: {
+    Map3F,
+    Map4F
+  }
+})
 export default class Venue extends Vue {
 }
 </script>
