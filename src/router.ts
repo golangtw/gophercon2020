cf23a9d4800089b0fe12789372d3292ac7cc372d
Aries Cs <aries0d0f@gmail.com>
2020-01-27 13:26:31 +0800

    [Deploy] Export route rules as module.

--- src/router.ts
+++ src/router.ts
@@ -11,61 +11,105 @@ import CFP from './pages/CFP.vue';
 
 Vue.use(Router);
 
+export const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      index: 0,
+      label: 'SITCON 2020',
+      menuItem: true
+    }
+  },
+  {
+    path: '/agenda',
+    name: 'Agenda',
+    component: Agenda,
+    meta: {
+      index: 1,
+      label: '議程資訊',
+      menuItem: true
+    }
+  },
+  {
+    path: '/agenda/:sid',
+    name: 'AgendaView',
+    component: Agenda,
+    meta: {
+      menuItem: false
+    }
+  },
+  {
+    path: '/activity',
+    name: 'Activity',
+    component: Activity,
+    meta: {
+      index: 0,
+      label: '活動',
+      menuItem: true
+    }
+  },
+  {
+    path: '/activity/:aid',
+    name: 'ActivityView',
+    component: Activity,
+    meta: {
+      menuItem: false
+    }
+  },
+  {
+    path: '/venue',
+    name: 'Venue',
+    component: Venue,
+    meta: {
+      index: 0,
+      label: '會場地圖',
+      menuItem: true
+    }
+  },
+  {
+    path: '/traffic',
+    name: 'Traffic',
+    component: Traffic,
+    meta: {
+      index: 0,
+      label: '交通方式',
+      menuItem: true
+    }
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: Team,
+    meta: {
+      index: 0,
+      label: 'SITCON 團隊',
+      menuItem: true
+    }
+  },
+  {
+    path: '/team/:tid',
+    name: 'TeamView',
+    component: Team,
+    meta: {
+      menuItem: false
+    }
+  },
+  {
+    path: '/cfp',
+    name: 'CFP',
+    component: CFP,
+    meta: {
+      menuItem: false
+    }
+  }
+];
+
 export default new Router({
   mode: 'history',
   base: '/2020/',
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/agenda',
-      name: 'Agenda',
-      component: Agenda
-    },
-    {
-      path: '/agenda/:sid',
-      name: 'AgendaView',
-      component: Agenda
-    },
-    {
-      path: '/activity',
-      name: 'Activity',
-      component: Activity
-    },
-    {
-      path: '/activity/:aid',
-      name: 'ActivityView',
-      component: Activity
-    },
-    {
-      path: '/venue',
-      name: 'Venue',
-      component: Venue
-    },
-    {
-      path: '/traffic',
-      name: 'Traffic',
-      component: Traffic
-    },
-    {
-      path: '/team',
-      name: 'Team',
-      component: Team
-    },
-    {
-      path: '/team/:tid',
-      name: 'TeamView',
-      component: Team
-    },
-    {
-      path: '/cfp',
-      name: 'CFP',
-      component: CFP
-    }
-  ],
+  routes,
   scrollBehavior (to, from, savedPosition) {
     if (to.name === 'CFP' && to.query.popUp && to.hash) {
       const target = document.querySelector(`.popup-content>${to.hash}`) as HTMLElement;
