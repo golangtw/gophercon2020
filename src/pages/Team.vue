d05c5a10f7b16129d4839dea949abc6a7879388e
Aries Cs <aries0d0f@gmail.com>
2020-01-27 21:46:37 +0800

    [Layout] Add under construction warning.

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="team" class="main-container"/>
+  <div id="team" class="main-container">
+    <h1 class="title"></h1>
+    <p class="content">
+      啊雷雷！？這個頁面還在施工中歐。<br/><br/>
+      <img src="https://img.moegirl.org/common/e/e9/%E8%A1%80%E5%B0%8F%E6%9D%BF.jpg" alt="">
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
