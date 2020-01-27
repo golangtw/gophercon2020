7edd9539ba22ef9f3c96a39be7546eaf3785d714
Aries Cs <aries0d0f@gmail.com>
2020-01-27 18:42:20 +0800

    [Layout] Activity bar.

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -1,11 +1,44 @@
 <template>
-  <div id="activity" class="main-container"/>
+  <div id="activity" class="main-container">
+    <h1 class="title">活動</h1>
+    <div class="activity-tabbar">
+      <div class="activity-head">
+        <p>活</p>
+        <p>動</p>
+      </div>
+      <div
+        v-for="activity in activities"
+        :key="`activity-${activity.name}`"
+        class="activity-tab"
+      >
+        <div class="label icon">
+          <img :src="activity.icon" :alt="activity.name"/>
+        </div>
+        <div class="image">
+          <img :src="activity.image" :alt="activity.name">
+        </div>
+        <div class="label name">
+          <p>{{ activity.name }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component({})
+import activityData from '@/../public/json/activity.json';
+
+interface ActivityPayload {
+  name: string;
+  description: string;
+  icon?: string;
+  image?: string;
+}
+
+@Component
 export default class Activity extends Vue {
+  private activities: ActivityPayload[] = activityData;
 }
 </script>
