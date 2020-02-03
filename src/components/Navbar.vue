4015a501f0515a2295e5481bed5c1747acff8b33
hyperbola <hyperbola.cs07@gmail.com>
2020-02-03 21:48:21 +0800

    [Layout] adjust navbar css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,36 @@
 <template>
-  <div
-    id="nav"
-    :class="{ 'popuped': isPopup }" 
-    class="navbar"
-  >
+  <div id="nav" :class="{ 'popuped': isPopup }" class="navbar">
     <nav class="menu-container">
       <div class="logo bar">
         <div class="logo-container">
-          <img src="/favicon.png" />
+          <img src="/favicon.png" class="navbar-icon" />
+          <p class="sitcon20">
+            <span class="title">SITCON</span>
+            <span class="subtitle">20</span>
+          </p>
         </div>
         <div class="title-container">
           <p class="title font-bold">SITCON</p>
           <p class="subtitle font-bold">2020</p>
         </div>
-        <div class="toggle-container" @click="isToggled = true">
-          <img src="@/assets/images/toggle-button.svg" />
+        <div class="toggle-container" @click="toggleMenu(!isMenuToggled)">
+          <img src="@/assets/images/toggle-button.svg" class="navbar-icon" />
         </div>
       </div>
-      <div class="menu" :class="{ toggle: isToggleMenu }">
+      <div class="menu" :class="{ toggle: isMenuToggled }">
         <div class="bar">
           <div class="logo-container">
-            <img src="/favicon.png" />
+            <img src="/favicon.png" class="navbar-icon" />
           </div>
           <div class="title-container">
-            <p class="title font-bold">define</p>
-            <p class="subtitle font-bold">
-              <span style="color: black">SITCON</span>20
+            <p class="define">define</p>
+            <p class="sitcon20">
+              <span class="title">SITCON</span>
+              <span class="subtitle">20</span>
             </p>
           </div>
-          <div class="toggle-container" @click="isToggled = false">
-            <img src="@/assets/images/toggle-button.svg" />
+          <div class="toggle-container" @click="toggleMenu(!isMenuToggled)">
+            <img src="@/assets/images/toggle-button.svg" class="navbar-icon" />
           </div>
         </div>
         <div
@@ -55,7 +56,7 @@ import { MenuItem } from '../store/types/menu';
 export default class Navbar extends Vue {
   @Action('toggleMenu', { namespace: 'menu' }) private toggleMenu!: () => void;
   @Getter('menu', { namespace: 'menu' }) private menu!: MenuItem[];
-  @Getter('toggle', { namespace: 'menu' }) private isToggleMenu!: boolean;
+  @Getter('toggle', { namespace: 'menu' }) private isMenuToggled!: boolean;
   @Getter('isPopup', { namespace: 'app' }) private isPopup!: boolean;
 }
 </script>
