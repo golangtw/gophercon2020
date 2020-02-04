cfb3db2f6b3b4211677d177c3cb22f34fe7b466e
hyperbola <hyperbola.cs07@gmail.com>
2020-02-04 13:30:47 +0800

    [Layout] simple sponsor section

---
+++ src/components/Sponsor.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="sponsor-container" id="sponsor">
+    <a href="#"><img src="@/assets/images/sponsors/coscup.png"></a>
+    <a href="#"><img src="@/assets/images/sponsors/sitcon.png"></a>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Sponsor extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.sponsor-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+
+  img {
+      max-height: 160px;
+  }
+}
+</style>
