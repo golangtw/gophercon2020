32bed7211dbf14084e2ccd80391810594ccc1f10
Denny Huang <denny0223@gmail.com>
2020-02-09 00:26:24 +0800

    Adjust home page button text

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,8 +6,8 @@
       <p class="date font-bold">3/28</p>
       <p class="location font-bold">中央研究院 — 人文社會科學館</p>
       <p class="kktix font-bold">
-        <a href="https://sitcon.kktix.cc/events/sitcon2020">報名</a>
-        <a href="https://sitcon.kktix.cc/events/sitcon2020-individual-sponsor">個人贊助</a>
+        <a href="https://sitcon.kktix.cc/events/sitcon2020">免費報名</a>
+        <a href="https://sitcon.kktix.cc/events/sitcon2020-individual-sponsor">贊助取票</a>
       </p>
     </header>
     <div class="spotlight-container">
@@ -29,7 +29,7 @@
         <p class="content">
           SITCON 冀望打造屬於學生自己的舞台，而我們也致力於為社群中的每位成員提供更友善、開放的環境。我們相信每位參與社群活動的夥伴都值得被尊重，而我們會盡力提供最安全的環境，讓參與 SITCON 盛會的每位朋友能夠認識個體間的差異、在社群中相互扶助、並鼓勵每個人揮灑屬於自己生命的色彩。<br/>
           因此，若有幸能在 SITCON 社群與您見面，無論您是以會眾、贊助商、工作人員、或是講者的身份參加活動，我們都希望您遵守以下的行為準則：
-          
+
           <ul>
             <li>尊重每一位參與者，將對方的感受放在心上。</li>
             <li>避免使用帶有侮辱、歧視、或具有潛在騷擾意涵的言語及手勢。</li>
@@ -48,7 +48,7 @@
             <li>干擾、攻擊會場網路，未經同意側錄封包或散佈隱私資訊。</li>
             <li>違反法律的行為。</li>
           </ul>
-          
+
           為了確保每位參與者的安全，我們將恪守以上規則。違反行為準則的參與者，我們將採取必要且合理的手段予以介入，包含但不限於將參與者請離現場、自相關名冊除名或列為不受歡迎對象，或在必要時移送警察機關法辦。若您或他人遇到了以上情況、或是有任何其他顧慮，都請立即尋求 SITCON 工作人員的協助。 我們誠摯的邀請來到 SITCON 的朋友一同履行以上承諾，將 SITCON 打造為更友善、更溫暖的所在；讓每一位參與者，都能在社群的交流中共同成長、在人與人的互動中找到自信——而這正是我們所珍惜的、屬於學生社群的真正精神。
         </p>
       </div>
