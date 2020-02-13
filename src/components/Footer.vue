b4720547ef11d349bf56dce6fc41db9dffa1906a
Aries Cs <aries0d0f@gmail.com>
2020-02-14 02:49:42 +0800

    [Fix] #master_cleanup `Footer` component and stylesheets.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,48 @@
 <template>
-  <footer id="footer" class="footer-container column-flex extend">
-    <div class="top-box extend column-flex">
-      <div id="brand" class="column-flex">
-        <p class="font-bold">學生計算機年會</p>
-        <p class="font-bold">Students' Information Technology Conference</p>
-      </div>
-      <div class="contact-and-media-container column-flex">
-        <div id="media" class="column-flex extend">
-          <p class="font-bold">
-            <a
-              v-for="media in communityMedia"
-              :key="`media-${ media.icon[1] }`"
-              :href="media.link"
-              target="_blank"
-              rel="noopener"
-              class="tone-trans"
-            >
-              <font-awesome-icon :icon="media.icon" />
-            </a>
-          </p>
-        </div>
-        <div id="contact" class="column-flex extend">
-          <p>
-            <a href="mailto:contact@sitcon.org" class="font-bold tone-trans">contact@sitcon.org</a>
-          </p>
-        </div>
-      </div>
+  <footer class="footer-container">
+    <hr class="footer-line">
+    <div id="brand" class="footer">
+      <p>
+        <span class="font-bold">學生計算機年會</span>
+        <span class="font-bold">Students' Information Technology Conference</span>
+      </p>
     </div>
-    <div id="past" class="column-flex extend">
-      <a
-        v-for="i in 7"
-        :key="`website-${2012 + i}`"
-        :href="`https://sitcon.org/${ 2012 + i }/`"
-        target="_blank"
-        rel="noopener"
-        class="font-bold tone-trans"
-      >{{ 2012 + i }}</a>
+    <div id="contact" class="footer">
+      <p>
+        <span class="font-bold">聯絡我們</span>
+        <span class="font-bold"><a href="mailto:contact@sitcon.org" class="font-bold">contact@sitcon.org</a></span>
+      </p>
+    </div>
+    <div id="past" class="footer">
+      <p>
+        <span class="font-bold">歷年網站</span>
+        <span class="font-bold">
+          <a
+            v-for="i in 7"
+            :key="`website-${2012 + i}`"
+            :href="`https://sitcon.org/${ 2012 + i }/`"
+            target="_blank"
+            rel="noopener"
+            class="font-bold"
+          >{{ 2012 + i }}</a>
+        </span>
+      </p>
+    </div>
+    <div id="media" class="footer">
+      <p>
+        <span class="font-bold">社群媒體</span>
+        <span class="font-bold">
+          <a
+            v-for="media in communityMedia"
+            :key="`media-${ media.icon[1] }`"
+            :href="media.link"
+            target="_blank"
+            rel="noopener"
+          >
+            <font-awesome-icon :icon="media.icon"/>
+          </a>
+        </span>
+      </p>
     </div>
   </footer>
 </template>
@@ -45,25 +52,26 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Footer extends Vue {
+
   private communityMedia = [
     {
-      icon: ['fab', 'youtube'],
+      icon: [ 'fab', 'youtube' ],
       link: 'https://sitcon.org/youtube'
     },
     {
-      icon: ['fab', 'facebook-f'],
+      icon: [ 'fab', 'facebook-f' ],
       link: 'https://sitcon.org/facebook'
     },
     {
-      icon: ['fab', 'twitter'],
+      icon: [ 'fab', 'twitter' ],
       link: 'https://sitcon.org/twitter'
     },
     {
-      icon: ['fab', 'telegram-plane'],
+      icon: [ 'fab', 'telegram-plane' ],
       link: 'https://sitcon.org/telegram'
     },
     {
-      icon: ['fab', 'flickr'],
+      icon: [ 'fab', 'flickr' ],
       link: 'https://sitcon.org/flickr'
     }
   ];
