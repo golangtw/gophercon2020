1b49cd2adf454d380b59d9e696b4852711e19e84
Aries Cs <aries0d0f@gmail.com>
2020-02-13 21:45:20 +0800

    [Layout] Add trafic flow graphic.

--- src/pages/Traffic.vue
+++ src/pages/Traffic.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="traffic" class="main-container">
+  <div id="traffic" class="traffic main-container">
     <h1 class="title">地點</h1>
     <p class="content">
       中央研究院人文社會科學館<br/>
       台北市南港區研究院路二段 128 號
     </p>
     <sitcon-map width="100%" height="650px" />
+    <div class="traffic-flow">
+      <img src="@/assets/images/traffic-flow.svg" alt="">
+    </div>
+    <div class="hint">
+      <p>
+        <font-awesome-icon :icon="['fas', 'angle-left']" />
+        <span>左右滑動路線圖以檢視更多</span>
+        <font-awesome-icon :icon="['fas', 'angle-right']" />
+      </p>
+    </div>
   </div>
 </template>
 
@@ -22,19 +32,3 @@ import SitconMap from '@/components/SitconMap.vue';
 export default class Traffic extends Vue {
 }
 </script>
-
-<style lang="scss" scoped>
-#traffic.main-container {
-  padding-left: 200px;
-  padding-right: 200px;
-
-  @media screen and (max-width: 1280px) {
-    padding-left: 50px;
-    padding-right: 50px;
-  }
-  @media screen and (max-width: 900px) {
-    padding-left: 16px;
-    padding-right: 16px;
-  }
-}
-</style>
