2a8e540328709f1d83a804dceebad099bb68d2cb
Aries Cs <aries0d0f@gmail.com>
2020-02-14 15:40:49 +0800

    [Content] Update contents.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -81,6 +81,78 @@
           為了確保每位參與者的安全，我們將恪守以上規則。違反行為準則的參與者，我們將採取必要且合理的手段予以介入，包含但不限於將參與者請離現場、自相關名冊除名或列為不受歡迎對象，或在必要時移送警察機關法辦。若您或他人遇到了以上情況、或是有任何其他顧慮，都請立即尋求 SITCON 工作人員的協助。 我們誠摯的邀請來到 SITCON 的朋友一同履行以上承諾，將 SITCON 打造為更友善、更溫暖的所在；讓每一位參與者，都能在社群的交流中共同成長、在人與人的互動中找到自信——而這正是我們所珍惜的、屬於學生社群的真正精神。
         </p>
       </div>
+      <div class="spotlight sharp-back-taton">
+        <h1 class="title has-prefix-icon">
+          <img class="prefix-icon" src="@/assets/images/sharp-white.svg" />
+          <span class="inner font-black">活動</span>
+          <span class="inner font-black">Activity</span>
+        </h1>
+        <p class="content">
+          延續過去有的雙向交流，今年我們有這些活動可以讓大家一起參與：
+
+          <ul>
+            <li>
+              <span>
+                天使計畫：<br>
+                天使計劃是一個提供會眾們交流機會的活動，在主辦方隨機配對後，讓有經驗的天使帶領初心者，協助他們瞭解這個年會。而這次也會繼續沿用去年的改變：依照天使的意願，除了天使與初心者一對一的交流，也會有一位天使帶兩位初心者的情況，讓兩位初心者能互相認識。
+              </span>
+            </li>
+            <li>
+              <span>
+                大會導遊團：<br>
+                第一次參加 Conference 嗎？第一次來到 SITCON 嗎？是個怕生的人嗎？?到了現場人山人海，不知道從何逛起？不管你是哪種，我們聽見你的聲音了。<br>
+                為了照顧第一次參與 Conference 的你，我們舉辦了 SITCON 導遊團，由走訪總召、幹部、講者、攤位，帶您一步步了解 SITCON 的各個面向、運作模式，並認識學生計算機年會的核心概念！
+              </span>
+            </li>
+            <li>
+              <span>
+                攤位活動：<br>
+                社群攤位的目的是在促進會眾跟學生社團或相關資訊社群交流。多元的活動讓你在聽完議程意猶未盡之餘，更能充分交流！休息時間，別忘記到場外的攤位逛逛！
+              </span>
+            </li>
+            <li>
+              <span>
+                Lightning Talk 閃電秀：<br>
+                Lightning Talk，中文譯為「閃電秀」，又稱 data blitz（資料閃電戰），顧名思義即為「閃電一般短的演講」。包含準備與連接投影機的時間在內，每位講者僅有 3 分鐘的時間。講者必須以飛快的速度，進行簡潔扼要的分享。要是沒能在 3 分鐘結束，會被直接切斷投影機訊號！無論是對台上的講者，或是台下的聽眾，都是十分刺激又轟動全場的體驗唷！Lightning Talk 將會在年會當天早上 10 點於白板區開放報名，先搶先贏，並在所有議程結束後、閉幕前，在國際會議廳進行演講。
+              </span>
+            </li>
+          </ul>
+
+          今年我們也新增了幾個特別活動：
+
+          <ul>
+            <li>
+              <span>
+                開放式議程：<br>
+                開放式議程跳脫過往講者對聽眾這種單向的資訊傳遞，期待藉由主講者與與會者的雙向交流，碰撞出更多的觀點與討論。在開放式議程，你可以參與一個圓桌會議，一起腦力激盪，甚至參與一場競賽！任何你想得到的非傳統議程形式都有機會在開放式議程中進行。
+              </span>
+            </li>
+          </ul>
+        </p>
+      </div>
+      <div class="spotlight sharp-back-taton">
+        <h1 class="title has-prefix-icon">
+          <img class="prefix-icon" src="@/assets/images/sharp-white.svg" />
+          <span class="inner font-black">問答</span>
+          <span class="inner font-black">Q &amp; A</span>
+        </h1>
+        <p class="content">
+          <ul>
+            <li>
+              <span>
+                Q1.　什麼樣的人適合參與年會？<br>
+                A1.　SITCON 學生計算機年會致力於資訊的推廣，無論是已經身在資訊領域，抑或是還沒踏進資訊領域的學生，只要有想一窺資訊世界的心都適合來參與！
+              </span>
+            </li>
+            <li>
+              <span>
+                Q2.　若有其他問題應該如何詢問？<br>
+                A2.　有任何問題都歡迎寄信至 <a href="mailto:contact@sitcon.org">contact@sitcon.org</a> 喔！
+              </span>
+            </li>
+          </ul>
+        </p>
+      </div>
     </div>
   </div>
 </template>
