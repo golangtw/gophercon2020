b7ec7d37c24a36e1828b5ecbf1377811c482900e
Vincent Ho <vincent.ho@umbocv.com>
2020-08-21 19:12:07 +0800

    remove unused sponsor and email section from the main page

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,41 @@
   <div
     id="app"
     :class="[
-      `theme-${ theme.toLowerCase() }`,
+      `theme-${theme.toLowerCase()}`,
       $route.name === 'CFP' ? 'cfp' : 'main',
       isInApp() ? 'in-app' : '',
-      isPopup ? 'popuped' : ''
+      isPopup ? 'popuped' : '',
     ]"
   >
-    <Navbar v-if="$route.name !== 'CFP' && !isInApp()"/>
+    <Navbar v-if="$route.name !== 'CFP' && !isInApp()" />
     <transition :name="transitionDirect.toLowerCase()">
       <keep-alive>
-        <router-view class="transition-group"/>
+        <router-view class="transition-group" />
       </keep-alive>
     </transition>
-    <Popup
-      :isOpen="isPopup"
-      :content="popupContent"
-    />
-    <SponsorFooter
-      :class="{ 'blur-filter': $route.name === 'Home' }"
-    />
-    <Footer/>
+    <Popup :isOpen="isPopup" :content="popupContent" />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
-import { Route } from 'vue-router';
+import { Route } from "vue-router";
 
-import { Component, Watch, Vue } from 'vue-property-decorator';
-import { Action, Getter } from 'vuex-class';
+import { Component, Watch, Vue } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
 
-import Navbar from '@/components/Navbar.vue';
-import Popup from '@/components/Popup.vue';
-import Footer from '@/components/Footer.vue';
-import SponsorFooter from '@/components/SponsorFooter.vue';
+import Navbar from "@/components/Navbar.vue";
+import Popup from "@/components/Popup.vue";
+import Footer from "@/components/Footer.vue";
 
-import { TemplateState } from '@/store/types/template';
-import { DeviceType, AppMode, ThemeType } from '@/store/types/app';
+import { TemplateState } from "@/store/types/template";
+import { DeviceType, AppMode, ThemeType } from "@/store/types/app";
 
-import head from '@/util/head';
+import head from "@/util/head";
 
 enum TransitionDirect {
-  SLIDE_LEFT = 'slide-left',
-  SLIDE_RIGHT = 'slide-right'
+  SLIDE_LEFT = "slide-left",
+  SLIDE_RIGHT = "slide-right",
 }
 
 @Component({
@@ -51,82 +44,91 @@ enum TransitionDirect {
     Navbar,
     Popup,
     Footer,
-    SponsorFooter
-  }
+  },
 })
 export default class App extends Vue {
-  @Action('toggleTheme', { namespace: 'app' }) private toggleTheme!: (theme: ThemeType) => void;
-  @Action('toggleMode', { namespace: 'app' }) private toggleMode!: (mode: AppMode) => void;
-  @Action('toggleDevice', { namespace: 'app' }) private toggleDevice!: (device: DeviceType) => void;
-  @Action('togglePopup', { namespace: 'app' }) private togglePopup!: (status: boolean) => void;
-  @Action('togglePopupContent', { namespace: 'app' }) private togglePopupContent!: (content: string) => void;
-  @Action('getSunRiseSunSetData', { namespace: 'sunRiseSunSet' }) private getSunRiseSunSetData!: () => Promise<void>;
-  @Getter('device', { namespace: 'app' }) private device!: DeviceType;
-  @Getter('mode', { namespace: 'app' }) private mode!: AppMode;
-  @Getter('theme', { namespace: 'app' }) private theme!: ThemeType;
-  @Getter('isPopup', { namespace: 'app' }) private isPopup!: boolean;
-  @Getter('popupContent', { namespace: 'app' }) private popupContent!: string;
-  @Getter('sunrise', { namespace: 'sunRiseSunSet' }) private sunrise!: Date;
-  @Getter('sunset', { namespace: 'sunRiseSunSet' }) private sunset!: Date;
-  @Getter('submitInfo', { namespace: 'template' }) private submitInfo!: string;
-  @Getter('openSubmit', { namespace: 'template' }) private openSubmit!: string;
-  @Getter('loudly', { namespace: 'template' }) private loudly!: string;
+  @Action("toggleTheme", { namespace: "app" }) private toggleTheme!: (
+    theme: ThemeType
+  ) => void;
+  @Action("toggleMode", { namespace: "app" }) private toggleMode!: (
+    mode: AppMode
+  ) => void;
+  @Action("toggleDevice", { namespace: "app" }) private toggleDevice!: (
+    device: DeviceType
+  ) => void;
+  @Action("togglePopup", { namespace: "app" }) private togglePopup!: (
+    status: boolean
+  ) => void;
+  @Action("togglePopupContent", { namespace: "app" })
+  private togglePopupContent!: (content: string) => void;
+  @Action("getSunRiseSunSetData", { namespace: "sunRiseSunSet" })
+  private getSunRiseSunSetData!: () => Promise<void>;
+  @Getter("device", { namespace: "app" }) private device!: DeviceType;
+  @Getter("mode", { namespace: "app" }) private mode!: AppMode;
+  @Getter("theme", { namespace: "app" }) private theme!: ThemeType;
+  @Getter("isPopup", { namespace: "app" }) private isPopup!: boolean;
+  @Getter("popupContent", { namespace: "app" }) private popupContent!: string;
+  @Getter("sunrise", { namespace: "sunRiseSunSet" }) private sunrise!: Date;
+  @Getter("sunset", { namespace: "sunRiseSunSet" }) private sunset!: Date;
+  @Getter("submitInfo", { namespace: "template" }) private submitInfo!: string;
+  @Getter("openSubmit", { namespace: "template" }) private openSubmit!: string;
+  @Getter("loudly", { namespace: "template" }) private loudly!: string;
 
   private transitionDirect: TransitionDirect = TransitionDirect.SLIDE_LEFT;
 
-  public async mounted () {
+  public async mounted() {
     this.autoDetectMetaOg();
     this.detectPopupFromLoadURL();
     this.assignThemePrefixToBody(this.theme);
     this.detectAppMode();
     this.detectDeviceType();
-    window.addEventListener('resize', this.detectDeviceType);
+    window.addEventListener("resize", this.detectDeviceType);
 
-    window.addEventListener('keyup', this.escHandler);
+    window.addEventListener("keyup", this.escHandler);
 
     await this.detectSystemPrefersColorSchema();
 
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(async () => {
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(async () => {
       await this.detectSystemPrefersColorSchema();
     });
   }
 
-  @Watch('$route')
-  public onChangeRoute (to: Route, from: Route) {
+  @Watch("$route")
+  public onChangeRoute(to: Route, from: Route) {
     this.autoDetectMetaOg();
     this.detectPopupFromLoadURL();
     this.detectTransitionDirect(to, from);
   }
 
-  @Watch('isPopup')
-  public onChangePopup (value: boolean) {
+  @Watch("isPopup")
+  public onChangePopup(value: boolean) {
     this.toggleBodyLock(value);
   }
 
-  @Watch('theme')
-  public onThemeChange (theme: ThemeType) {
+  @Watch("theme")
+  public onThemeChange(theme: ThemeType) {
     this.assignThemePrefixToBody(theme);
   }
 
-  public destroyed () {
-    window.removeEventListener('resize', this.detectDeviceType);
+  public destroyed() {
+    window.removeEventListener("resize", this.detectDeviceType);
   }
 
-  private isInApp (): boolean {
+  private isInApp(): boolean {
     return this.mode === AppMode.APP;
   }
 
-  private detectedEgg (): boolean {
+  private detectedEgg(): boolean {
     const now: Date = new Date();
 
     return (
-      now.getTime() > new Date('2019-10-26').getTime() &&
-      now.getTime() < new Date('2019-11-27').getTime()
+      now.getTime() > new Date("2019-10-26").getTime() &&
+      now.getTime() < new Date("2019-11-27").getTime()
     );
   }
 
-  private detectAppMode (): void {
-    const isApp: boolean = this.$route.query.mode === 'app';
+  private detectAppMode(): void {
+    const isApp: boolean = this.$route.query.mode === "app";
 
     if (isApp) {
       this.toggleMode(AppMode.APP);
@@ -135,8 +137,8 @@ export default class App extends Vue {
     }
   }
 
-  private detectDeviceType (): void {
-    const isDesktop: boolean = window.matchMedia('(min-width: 1024px)').matches;
+  private detectDeviceType(): void {
+    const isDesktop: boolean = window.matchMedia("(min-width: 1024px)").matches;
 
     if (isDesktop) {
       this.toggleDevice(DeviceType.DESKTOP);
@@ -145,12 +147,12 @@ export default class App extends Vue {
     }
   }
 
-  private async detectSystemPrefersColorSchema (): Promise<void> {
+  private async detectSystemPrefersColorSchema(): Promise<void> {
     const isDarkMode: boolean = window.matchMedia(
-      '(prefers-color-scheme: dark)'
+      "(prefers-color-scheme: dark)"
     ).matches;
     const isLightMode: boolean = window.matchMedia(
-      '(prefers-color-scheme: light)'
+      "(prefers-color-scheme: light)"
     ).matches;
 
     if (isLightMode) {
@@ -171,61 +173,62 @@ export default class App extends Vue {
     }
   }
 
-  private toggleThemeViaSunRiseSunSet (): void {
+  private toggleThemeViaSunRiseSunSet(): void {
     const now = new Date();
     const isEgg: boolean = this.detectedEgg();
     const isDay: boolean =
       now.getTime() > this.sunrise.getTime() &&
       now.getTime() < this.sunset.getTime();
 
-    let themePrefix: string = '';
+    let themePrefix: string = "";
 
     if (isEgg) {
-      themePrefix = 'RAINBOW_';
+      themePrefix = "RAINBOW_";
     } else {
-      themePrefix = '';
+      themePrefix = "";
     }
 
     if (isDay) {
-      themePrefix += 'LIGHT';
+      themePrefix += "LIGHT";
     } else {
-      themePrefix += 'DARK';
+      themePrefix += "DARK";
     }
 
     this.toggleTheme(ThemeType[themePrefix as keyof typeof ThemeType]);
   }
 
-  private assignThemePrefixToBody (theme: ThemeType): void {
-    const bodyDOM = (document.querySelector('body') as HTMLElement);
-    bodyDOM.classList.remove('theme-dark');
-    bodyDOM.classList.remove('theme-light');
-    bodyDOM.classList.remove('theme-rainbow-dark');
+  private assignThemePrefixToBody(theme: ThemeType): void {
+    const bodyDOM = document.querySelector("body") as HTMLElement;
+    bodyDOM.classList.remove("theme-dark");
+    bodyDOM.classList.remove("theme-light");
+    bodyDOM.classList.remove("theme-rainbow-dark");
     bodyDOM.classList.add(`theme-${theme}`);
   }
 
-  private detectPopupFromLoadURL (): void {
+  private detectPopupFromLoadURL(): void {
     if (this.$route.query && this.$route.query.popUp) {
       switch (this.$route.query.popUp) {
-        case 'submitInfo':
+        case "submitInfo":
           this.togglePopup(true);
           this.togglePopupContent(this.submitInfo);
           break;
-        case 'openSubmit':
+        case "openSubmit":
           this.togglePopup(true);
           this.togglePopupContent(this.openSubmit);
           break;
-        case 'loudly':
+        case "loudly":
           this.togglePopup(true);
           this.togglePopupContent(this.loudly);
           break;
-        default: break;
+        default:
+          break;
       }
-    } else if (!this.$route.name!.includes('Agenda')) {
+    } else if (!this.$route.name!.includes("Agenda")) {
       this.togglePopup(false);
     }
   }
 
-  private detectTransitionDirect (to: Route, from: Route): void {
+  private detectTransitionDirect(to: Route, from: Route): void {
     if (to.meta.index < from.meta.index) {
       this.transitionDirect = TransitionDirect.SLIDE_RIGHT;
     } else {
@@ -233,58 +236,71 @@ export default class App extends Vue {
     }
   }
 
-  private toggleBodyLock (isLock: boolean): void {
+  private toggleBodyLock(isLock: boolean): void {
     if (isLock) {
-      (document.querySelector('body') as HTMLElement).classList.add('popup-scrolling-lock');
+      (document.querySelector("body") as HTMLElement).classList.add(
+        "popup-scrolling-lock"
+      );
     } else {
-      (document.querySelector('body') as HTMLElement).classList.remove('popup-scrolling-lock');
+      (document.querySelector("body") as HTMLElement).classList.remove(
+        "popup-scrolling-lock"
+      );
     }
   }
 
-  private escHandler (event: any): void {
+  private escHandler(event: any): void {
     if (event.keyCode === 27 && this.isPopup) {
       this.togglePopup(false);
     }
   }
 
-  private autoDetectMetaOg (): void {
+  private autoDetectMetaOg(): void {
     switch (this.$route.name) {
-      case 'Agenda':
-        head.title('議程表');
-        head.ogTitle('議程表');
-        head.ogDescription('GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。');
-        head.ogUrl('https://gophercon.golang.tw/2020/agenda/');
-        head.ogImage('https://gophercon.golang.tw/2020/img/og.png');
+      case "Agenda":
+        head.title("議程表");
+        head.ogTitle("議程表");
+        head.ogDescription(
+          "GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。"
+        );
+        head.ogUrl("https://gophercon.golang.tw/2020/agenda/");
+        head.ogImage("https://gophercon.golang.tw/2020/img/og.png");
 
         break;
 
-      case 'Traffic':
-        head.title('交通方式');
-        head.ogTitle('交通方式');
-        head.ogDescription('GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。');
-        head.ogUrl('https://gophercon.golang.tw/2020/traffic/');
-        head.ogImage('https://gophercon.golang.tw/2020/img/og.png');
+      case "Traffic":
+        head.title("交通方式");
+        head.ogTitle("交通方式");
+        head.ogDescription(
+          "GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。"
+        );
+        head.ogUrl("https://gophercon.golang.tw/2020/traffic/");
+        head.ogImage("https://gophercon.golang.tw/2020/img/og.png");
 
         break;
 
-      case 'Team':
-      case 'Team/Staff':
-      case 'Team/Sponsor':
-        head.title('GopherCon團隊');
-        head.ogTitle('GopherCon團隊');
-        head.ogDescription('GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。');
-        head.ogUrl('https://gophercon.golang.tw/2020/team/staff');
-        head.ogImage('https://gophercon.golang.tw/2020/img/og.png');
-      case 'Venue':
-        head.title('會場地圖');
-        head.ogTitle('會場地圖');
-        head.ogDescription('GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。');
-        head.ogUrl('https://gophercon.golang.tw/2020/traffic/');
-        head.ogImage('https://gophercon.golang.tw/2020/img/og.png');
+      case "Team":
+      case "Team/Staff":
+      case "Team/Sponsor":
+        head.title("GopherCon團隊");
+        head.ogTitle("GopherCon團隊");
+        head.ogDescription(
+          "GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。"
+        );
+        head.ogUrl("https://gophercon.golang.tw/2020/team/staff");
+        head.ogImage("https://gophercon.golang.tw/2020/img/og.png");
+      case "Venue":
+        head.title("會場地圖");
+        head.ogTitle("會場地圖");
+        head.ogDescription(
+          "GopherCon TW 2020 邀請身為 Golang 使用者的你，向大家分享您的經驗與技術，期待您能在演講桌前，與我們一起 have fun with golang。"
+        );
+        head.ogUrl("https://gophercon.golang.tw/2020/traffic/");
+        head.ogImage("https://gophercon.golang.tw/2020/img/og.png");
 
         break;
 
-      default: break;
+      default:
+        break;
     }
   }
 }
