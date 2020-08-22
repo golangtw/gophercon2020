dd290af87418673ecaf47f48d79cc9946dbfabb6
reJoker <djack0812@gmail.com>
2020-08-22 12:46:56 +0800

    feat(LogoTop): add LogoTop component
    Creeate the LogoTop compoent which will be fixed on top of the pages.

---
+++ src/components/LogoTop.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="logo-top">
+    <router-link to="/">
+      <img src="@/assets/images/logo.svg">
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/mixin.scss';
+
+  .logo-top {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    top: 24px;
+    z-index: 1009;
+    height: 0;
+
+    a {
+        position: relative;
+    }
+    img {
+        @include for-size(xs) {
+            width: 200px;
+            height: 67px;
+        }
+        @include for-size(sm) {
+            width: 200px;
+            height: 67px;
+        }
+        @include for-size(md) {
+            width: 300px;
+            height: 100px;
+        }
+        @include for-size(lg) {
+            width: 300px;
+            height: 100px;
+        }
+        @include for-size(xl) {
+            width: 457px;
+            height: 153px;
+        }
+    }
+  }
+
+</style>
