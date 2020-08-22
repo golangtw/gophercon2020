f53df2d05b3f43421e08e56747ffc496a0e55b5a
Flynn Hou <flynnhou@gmail.com>
2020-08-22 11:29:26 +0800

    fix(footer): revamp the style with rwd

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,64 +1,60 @@
 <template>
-  <footer>
-    <div class="footer-container">
-      <div id="brand" class="footer">
-        <p>
-          <span class="font-bold">GopherCon TW</span>
-          <span class="font-bold">Gopher Conference Taiwan</span>
-        </p>
+  <div class="g-footer">
+    <div v-if="showSponsors" class="g-footer-sponsors">
+      <div class="sponsor">
+        <img src="/img/sponsors/chungyo.png" alt="chung yo group" />
       </div>
-      <div id="contact" class="footer">
-        <p>
-          <span class="font-bold">Contact 聯絡我們</span>
-          <span class="font-bold"
-            ><a href="mailto:contact@golang.tw" class="font-bold"
-              >contact@golang.tw</a
-            ></span
-          >
-        </p>
-      </div>
-      <div id="past" class="footer">
-        <p>
-          <span class="font-bold">歷年網站</span>
-          <span class="font-bold">
-            <a
-              v-for="i in 1"
-              :key="`website-${2019 + i}`"
-              :href="`https://gophercon.golang.tw/${2019 + i}/`"
-              target="_blank"
-              rel="noopener"
-              class="font-bold"
-              >{{ 2019 + i }}</a
-            >
-          </span>
-        </p>
-      </div>
-      <div id="media" class="footer">
-        <p>
-          <span class="font-bold">社群媒體</span>
-          <span class="font-bold">
-            <a
-              v-for="media in communityMedia"
-              :key="`media-${media.icon[1]}`"
-              :href="media.link"
-              target="_blank"
-              rel="noopener"
-            >
-              <font-awesome-icon :icon="media.icon" />
-            </a>
-          </span>
-        </p>
+      <div class="sponsor">
+        <img src="/img/sponsors/ocf.png" alt="open culture foundation" />
       </div>
     </div>
-  </footer>
+    <div class="g-footer-socialMedia">
+      <div class="g-footer-socialMedia--text">
+        Contact us in one of the following social networks
+      </div>
+      <div class="g-footer-socialMedia--icons">
+        <div class="social-media-icon">
+          <a href="mailto:contact@golang.tw">
+            <EmailIcon />
+          </a>
+        </div>
+        <div
+          v-for="s in socialMedia"
+          :key="`media-${s.icon[1]}`"
+          class="social-media-icon"
+        >
+          <a :href="s.link" target="_blank" rel="noopener">
+            <font-awesome-icon :icon="s.icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="g-footer-copyright">
+      <div class="g-footer-copyright--image">
+        <LogoBottom />
+      </div>
+      <div class="g-footer-copyright--text">
+        Copyright © 2010-2020 GopherCon Taiwan. All rights reserved.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 
-@Component
+import LogoBottom from "./LogoBottom.vue";
+import EmailIcon from "./icons/Email.vue";
+
+@Component({
+  components: {
+    EmailIcon,
+    LogoBottom,
+  },
+})
 export default class Footer extends Vue {
-  private communityMedia = [
+  @Prop() private showSponsors!:boolean;
+  private socialMedia = [
     {
       icon: ["fab", "meetup"],
       link: "https://www.meetup.com/golang-taipei-meetup",
@@ -68,7 +64,7 @@ export default class Footer extends Vue {
       link: "https://github.com/golangtw",
     },
     {
-      icon: ["fab", "facebook-f"],
+      icon: ["fab", "facebook"],
       link: "https://www.facebook.com/golangtaipei/",
     },
     {
@@ -76,13 +72,138 @@ export default class Footer extends Vue {
       link: "https://t.me/golangtw",
     },
     {
-      icon: ["fab", "youtube"],
-      link: "https://www.youtube.com/channel/UCjnIy_GQAuszZurUIO3zg9Q",
-    },
-    {
       icon: ["fab", "twitter"],
       link: "https://twitter.com/golangtw",
+    },
+    {
+      icon: ["fab", "youtube"],
+      link: "https://www.youtube.com/channel/UCjnIy_GQAuszZurUIO3zg9Q",
     },
   ];
 }
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+
+@mixin iconColor($color) {
+  path {
+    fill: $color;
+  }
+}
+
+.g-footer {
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+
+  &-sponsors {
+    display: flex;
+    padding: 80px 48px;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    background: #ffffff;
+
+    & > .sponsor {
+      display: flex;
+      width: 260px;
+      margin: 20px 40px;
+      justify-content: center;
+
+      img {
+        min-height: 64px;
+        max-height: 80px;
+        object-fit: contain;
+
+        @media (max-width: $screen-md) {
+          min-height: 56px;
+          max-height: 64px;
+        }
+      }
+    }
+  }
+
+  &-socialMedia {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 40px 0 100px;
+    background: #e8e8e8;
+
+    &--text {
+      display: flex;
+      margin-top: 40px;
+      padding: 0 16px;
+      justify-content: center;
+      color: #343942;
+      text-align: center;
+    }
+
+    &--icons {
+      display: flex;
+      max-width: $screen-md;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: flex-start;
+
+      & > .social-media-icon {
+        width: 45px;
+        height: 45px;
+        margin: 24px 32px;
+        border-radius: 50%;
+
+        & > a {
+          width: 100%;
+          height: 100%;
+
+          & > svg {
+            width: 100%;
+            height: 100%;
+
+            &.fa-meetup {
+              @include iconColor(#ea3340);
+            }
+
+            &.fa-facebook {
+              @include iconColor(#2e76f3);
+            }
+
+            &.fa-telegram-plane {
+              @include iconColor(#49a7da);
+            }
+
+            &.fa-twitter {
+              @include iconColor(#4caeef);
+            }
+
+            &.fa-youtube {
+              @include iconColor(#eb3323);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  &-copyright {
+    padding: 40px 16px;
+    background: #4e687e;
+
+    &--image {
+      display: flex;
+      margin-top: 40px;
+      justify-content: center;
+    }
+
+    &--text {
+      display: flex;
+      margin-top: 40px;
+      justify-content: center;
+      color: #bbfad7;
+      text-align: center;
+    }
+  }
+}
+</style>
---
+++ src/components/icons/Email.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="45"
+    height="45"
+    viewBox="0 0 45 45"
+  >
+    <g fill="none" fill-rule="evenodd">
+      <circle cx="22.5" cy="22.5" r="22.5" fill="#476880" />
+      <g stroke-width="1.8" transform="translate(9 13)">
+        <rect
+          width="24.3"
+          height="17.1"
+          x="1.8"
+          y=".9"
+          fill="#F7F8F9"
+          stroke="#F7F8F9"
+          rx="2.25"
+        />
+        <path
+          stroke="#476880"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M0.853 0.45L13.95 9.45 27.047 0.45"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
