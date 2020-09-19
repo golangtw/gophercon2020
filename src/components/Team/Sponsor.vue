4cd7e2c8396cbb849a5570e09029f582c7d68b31
David Chou <david.chou@umbocv.com>
2020-09-20 01:27:20 +0800

    chore(sponsor): remove personal sponsor related info

--- src/components/Team/Sponsor.vue
+++ src/components/Team/Sponsor.vue
@@ -1,29 +1,6 @@
 <template>
   <div id="sponsor">
     <div class="sponsor-container">
-      <div class="card-container individual-sponsor">
-        <div class="card">
-          <h2 class="font-black subtitle">我們歡迎個人贊助</h2>
-          <p class="paragraph">Gopher Conference 為 Golang Taipei 自發組成籌辦的研討會，秉持著以社群成員為主軸的核心價值，提供一個經驗交流與資訊技術實務分享的平台。</p>
-          <p class="paragraph">
-            維繫這樣的交流平台，需要多人的努力及貢獻，亦十分需要在資金上的支持。
-            <br />希望能藉由您的一份力量，來促成活動的舉行，讓更多 Golang 使用者能在舞台上展現自我成長。
-            <br />歡迎您參與贊助，支持 GopherCon、讓我們將每年的活動辦得盡善盡美！
-          </p>
-          <p class="paragraph">
-            <a
-              href="https://bit.ly/donategophercon2020"
-              target="_blank"
-              rel="noopener"
-              class="font-bold"
-            >個人贊助辦法</a>
-          </p>
-          <p class="paragraph">
-            企業贊助亦歡迎來信
-            <a class="font-bold" href="mailto:contact@golang.tw">contact@golang.tw</a>
-          </p>
-        </div>
-      </div>
       <div
         v-for="level in Object.keys(sponsorList)"
         :key="`sponsor-level-${level}`"
