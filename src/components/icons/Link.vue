32c8f48cdc009afbc4c490a6f0539f996d3bbccc
Flynn Hou <flynnhou7@gmail.com>
2020-10-06 05:44:08 +0800

    fix(sponsor): revamp to match the rwd design spec

---
+++ src/components/icons/Link.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+  >
+    <g fill="none" fill-rule="evenodd">
+      <g fill="#61C3FF" fill-rule="nonzero">
+        <path
+          d="M13.455 2H9.09c-.301 0-.546.244-.546.545 0 .302.245.546.546.546h3.047L6.523 8.705c-.213.213-.213.559 0 .772.107.106.246.16.386.16s.28-.054.386-.16l5.614-5.615V6.91c0 .301.244.546.546.546.3 0 .545-.245.545-.546V2.545c0-.3-.244-.545-.545-.545z"
+          transform="translate(-412 -1156) translate(292 216) translate(0 411) translate(40 40) translate(0 326) translate(0 145) translate(80 18)"
+        />
+        <path
+          d="M11.273 7.455c-.302 0-.546.244-.546.545v4.91H3.091V5.272H8c.301 0 .545-.244.545-.546 0-.301-.244-.545-.545-.545H2.545c-.3 0-.545.244-.545.545v8.728c0 .3.244.545.545.545h8.728c.301 0 .545-.244.545-.545V8c0-.301-.244-.545-.545-.545z"
+          transform="translate(-412 -1156) translate(292 216) translate(0 411) translate(40 40) translate(0 326) translate(0 145) translate(80 18)"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
